03ebf8a3744265fef3c85145dff2d3b9775e0a12
Michal <michal.biernat@currenda.pl>
2022-02-17 15:47:18 +0100

    WIP: build layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Thank you for considering participating to this paper" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import * as data from "@/assets/js/frontiers-metropolis-challenge-L1.json";
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  protected initialData: any;
+
+  constructor() {
+    super();
+    this.initialData = data;
+  }
+}
 </script>

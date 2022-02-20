2212e7ee70557be781d132f1cb58125546afe400
Michal <michal.biernat@currenda.pl>
2022-02-20 12:36:11 +0100

    build and animate logo container

---
+++ src/components/LogoContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="logo__container">
+    <div class="animation__container">
+      <div class="paper__container">
+        <div class="firstPaper"></div>
+        <div class="secondPaper"></div>
+        <div class="paper__sheet">
+          <div class="paper__arrow"></div>
+
+          <div class="success-checkmark"></div>
+          <div class="paper__content">
+            <div class="paper__content__line"></div>
+            <div class="paper__content__line"></div>
+            <div class="paper__content__line"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="horizontalLine"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+export default class LogoContainer extends Vue {}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <LogoContainer />
     <TitleContainer msg="Thank you for considering participating to this paper" />
     <Card :data="initialData" />
   </div>
@@ -10,12 +10,14 @@
 import { Component, Vue } from "vue-property-decorator";
 import TitleContainer from "@/components/typography/TitleContainer.vue";
 import Card from "@/components/Card.vue";
+import LogoContainer from "@/components/LogoContainer.vue";
 import * as data from "@/assets/js/frontiers-metropolis-challenge-L1.json";
 
 @Component({
   components: {
     TitleContainer,
     Card,
+    LogoContainer,
   },
 })
 export default class HomeView extends Vue {
